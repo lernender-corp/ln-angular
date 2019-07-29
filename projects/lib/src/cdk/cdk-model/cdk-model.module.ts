a6426247ced56f083dd1ce1616a8ce9227713089
Shawn Lovelidge <shawnlovelidge@gmail.com>
2019-07-29 10:55:41 -0500

    Fixed Build

--- projects/lib/src/cdk/cdk-model/cdk-model.module.ts
+++ projects/lib/src/cdk/cdk-model/cdk-model.module.ts
@@ -10,29 +10,13 @@ import { AuthenticationConfig } from './authentication/authentication-config';
 import { Carousel } from './common/carousel';
 import { CdkDataSource } from './generic/datasource';
 import { Container } from './generic/container';
-import { Engine } from './vehicle/engine';
-import { Environment } from './common/environment';
-
-import { Flags } from './vehicle/flags';
+import { Environment } from './service/environment';
 import { Grid } from './tabular/grid';
-
-import { MPG } from './vehicle/mpg';
-import { Media } from './vehicle/media';
-import { Model } from './vehicle/model';
 import { Node } from './common/node';
-import { Option } from './vehicle/option';
 import { PageScrollInstance } from './page-scroll/page-scroll-instance';
 import { Pane } from './generic/pane';
 import { Panel } from './generic/panel';
-import { Price } from './vehicle/price';
-import { Promote } from './vehicle/promote';
-import { SafetyRating } from './vehicle/safetyRating';
 import { SlidePanel } from './generic/slide-panel';
-import { StandardOption } from './vehicle/standard-option';
-import { Transmission } from './vehicle/transmission';
-import { Vehicle } from './vehicle/vehicle';
-import { Vspec } from './vehicle/vspec';
-
 
 
 const CDK_MODELS = [
@@ -40,28 +24,13 @@ const CDK_MODELS = [
   Carousel,
   CdkDataSource,
   Container,
-  Engine,
   Environment,
-
-  Flags,
   Grid,
-
-  MPG,
-  Media,
-  Model,
   Node,
-  Option,
   PageScrollInstance,
   Pane,
   Panel,
-  Price,
-  Promote,
-  SafetyRating,
   SlidePanel,
-  StandardOption,
-  Transmission,
-  Vehicle,
-  Vspec
 ];
 
 @NgModule({
